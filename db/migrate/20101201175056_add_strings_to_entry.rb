55cab79ab106980668abca77392dc748965a22a3
hokiecanada <hokiecanada@hotmail.com>
2010-12-02 16:36:04 -0500

    d

--- db/migrate/20101201175056_add_strings_to_entry.rb
+++
@@ -1,9 +0,0 @@
-class AddStringsToEntry < ActiveRecord::Migration
-  def self.up
-    add_column :entries, :tags_string, :string
-  end
-
-  def self.down
-    remove_column :entries, :tags_string
-  end
-end
--- db/migrate/20101202194445_add_author_string_to_entry.rb
+++
@@ -1,9 +0,0 @@
-class AddAuthorStringToEntry < ActiveRecord::Migration
-  def self.up
-    add_column :entries, :authors_string, :string
-  end
-
-  def self.down
-    remove_column :entries, :authors_string
-  end
-end
