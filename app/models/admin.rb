e44878b984d9e23d9ae8acba3118e5d8d5c2bcdd
hokiecanada <hokiecanada@hotmail.com>
2010-12-06 17:48:05 -0500

    .

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ActiveRecord::Base
 
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
