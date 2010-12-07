60b8f79851721b5a170b411f2b57d1a1cba3649e
hokiecanada <hokiecanada@hotmail.com>
2010-12-06 19:04:19 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
  
   before_filter :tag_cloud
   before_filter :get_keyword
-  before_filter :get_tags
   
   def tag_cloud
 	@tags = (Entry.tag_counts_on(:authors) + Entry.tag_counts_on(:comps) + Entry.tag_counts_on(:systems) + Entry.tag_counts_on(:tasks)).sort_by {rand}
@@ -13,12 +12,6 @@ class ApplicationController < ActionController::Base
   
   def get_keyword
 	@keyword = params[:keyword]
-  end
-	
-  def get_tags
-	@components = Component.find(:all)
-	@systems = System.find(:all)
-	@tasks = Task.find(:all)
   end
   
 end
